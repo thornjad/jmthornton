5830d03a956029258229a20cf5f7c375229a6221
Jade Michael Thornton <jade@jmthornton.net>
2017-11-04 16:45:12 -0500

    convert most functions to NewsPosts object

--- tools/news/assets/script/news.js
+++ tools/news/assets/script/news.js
@@ -1,63 +1,53 @@
 'use strict';
 
-const apiURI = 'https://hacker-news.firebaseio.com/v0/';
-const firstPageLoadLimit = 30;
-
-let topPosts = [];
-
-const formatLayout = (title, url, source) => {
-  return `
-  <div class="post">
-    <hr>
-    <p>
-      <a href="${url}">${title}</a>, (${source})
-    </p>
-  </div>
-  `;
-}
-
-async function setTop() {
-  topPosts = JSON.parse(await getTop500());
-}
-
-const getTop500 = () => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = (data) => resolve(xhr.responseText);
-    xhr.onerror = (e) => reject(xhr.errorText);
-    xhr.open('GET', `${apiURI}topstories.json`);
-    xhr.send();
-  });
-}
-
-function* getPostList() {
-  while (topPosts.length > 0) {
-    yield topPosts[0];
-    topPosts = topPosts.removeElement(0);
+class NewsPosts {
+  constructor() {
+    this.apiURI = 'https://hacker-news.firebaseio.com/v0/';
+    this.firstPageLoadLimit = 30;
+    this.allPosts = null;
+    this.genPost = null;
   }
-}
 
-const getPostData = (id) => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = (data) => resolve(xhr.responseText);
-    xhr.onerror = (e) => reject(xhr.errorText);
-    xhr.open('GET', `${apiURI}item/${id}.json`);
-    xhr.send();
-  });
-}
-
-async function loadFirst() {
-  let posts = getPostList();
-  let firstPostsFuture = [];
-
-  await setTop();
-  for (let i = 0; i < firstPageLoadLimit; i++) {
-    firstPostsFuture.push(getPostData(posts.next().value));
+  async init() {
+    this.allPosts = JSON.parse(await this.getTop500());
+    this.genPost = NewsPosts.postList(this.allPosts);
   }
-  const firstPostsList = await Promise.all(firstPostsFuture);
-  for (let post of firstPostsList) {
-    post = JSON.parse(post);
-    console.log(post.title);
+
+  getTop500() {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = (data) => resolve(xhr.responseText);
+      xhr.onerror = (e) => reject(xhr.errorText);
+      xhr.open('GET', `${this.apiURI}topstories.json`);
+      xhr.send();
+    });
+  }
+
+  getPostData(id) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = (data) => resolve(xhr.responseText);
+      xhr.onerror = (e) => reject(xhr.errorText);
+      xhr.open('GET', `${this.apiURI}item/${id}.json`);
+      xhr.send();
+    });
+  }
+
+  static formatLayout(title, url, source) {
+    return `
+    <div class="post">
+      <hr>
+      <p>
+        <a href="${url}">${title}</a>, (${source})
+      </p>
+    </div>
+    `;
+  }
+
+  static * postList(lists) {
+    while (lists.length > 0) {
+      yield lists[0];
+      lists = lists.removeElement(0);
+    }
   }
 }
