ba80af24f18236bced7f7e941942756f03dad621
Jade Michael Thornton <jademichael@jmthornton.net>
2023-06-28 10:06:02 -0400

    improve ranking algorithm

--- tools/libertarian-zoo/zoo.js
+++ tools/libertarian-zoo/zoo.js
@@ -4,7 +4,7 @@ function mountCandidates(mountpoint, candidates) {
   const votesNF = new Intl.NumberFormat();
   const percentNF = new Intl.NumberFormat(undefined, { style: 'percent' });
   const totalVotes = candidates.reduce((t, c) => t + c.votes, 0);
-  candidates = candidates.map(c => ({...c, rank: rank(c)}));
+  candidates = candidates.map(c => ({...c, pollMedian: median(c.polls)}));
 
   candidates
     .sort(sortCandidates)
@@ -17,10 +17,10 @@ function mountCandidates(mountpoint, candidates) {
         info = 'No additional information available at this time',
         votes: rawVotes = 0,
         dropped = false,
+        pollMedian = 0,
       } = c;
 
       const votes = votesNF.format(rawVotes);
-      const rank = votesNF.format(~~Math.round((1 - c.rank) * 50))
       const place = dropped ? '' : `#${i + 1}`;
       const rawPercent = rawVotes / totalVotes;
       const percent = (rawPercent < 0.01 && rawPercent > 0)
@@ -45,7 +45,7 @@ function mountCandidates(mountpoint, candidates) {
                 ${dropped ? 'Dropped out' : ''}
               </em></small>
               <small class="popular-vote">
-                Zoo Ranking: ${rank} | Pledged Delegates: ${votes} ${isNaN(percent) ? '' : '(' + percent + ')'}
+                Median Polling: ${pollMedian} | Pledged Delegates: ${votes} ${isNaN(percent) ? '' : '(' + percent + ')'}
               </small>
             </h3>
           </article>
@@ -55,32 +55,19 @@ function mountCandidates(mountpoint, candidates) {
     });
 }
 
-const rank = (candidate) => {
-  // Returns the relative rank for the candidate, ranging from -1 to 1, where -1 is the most
-  // favorable. Rank is penalized for dropping out, not yet having officially declared, whether the
-  // candidate is "serious", and the median poll percentage (being votes / total votes)
-  let algoRank = 0;
-  let pollRank = 0;
-  if (!candidate.serious) {
-    algoRank = 0.5;
-  }
-  if (candidate.dropped) {
-    algoRank = 0.9;
-  }
-  if (candidate.polls?.length) {
-    // convert to -1-1 scale
-    pollRank = median(candidate.polls) / 50 - 1;
-  }
-  console.log(algoRank, pollRank)
-  return (algoRank + pollRank) / 2
-};
-
 const sortCandidates = (a, b) => {
-  return (a.rank - b.rank) || a.sortName.localeCompare(b.sortName);
+  const dropped = Number(b.dropped ?? 0) - Number(a.dropped ?? 0);
+  const votes = (a.votes - b.votes) || 0;
+  const serious = Number(b.serious ?? 0) - Number(a.serious ?? 0);
+  const polls = b.pollMedian - a.pollMedian;
+  // Sorting in order of importance of these various factors
+  return dropped || votes || serious || polls || a.sortName.localeCompare(b.sortName);
 };
 
-function median(xs) {
-  const len = xs.sort().length;
+function median(xs = []) {
+  const len = xs.length;
+  if (!len) return 0;
   const half = Math.floor(len / 2);
+  xs.sort();
   return len % 2 ? xs[half] : (xs[half - 1] + xs[half]) / 2.0;
 }
