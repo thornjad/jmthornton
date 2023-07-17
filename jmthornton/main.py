e690b4ac7c09cd60957b7da1c24eb4a46ec8693e
Jade Michael Thornton <jademichael@jmthornton.net>
2023-07-16 23:21:02 -0400

    add new routes and update existing ones

--- jmthornton/main.py
+++ jmthornton/main.py
@@ -14,7 +14,49 @@ def root_index():
 @app.route("/tools/")
 def tools():
     """Tools root."""
-    pass
+    return render_template("tools/index.html")
+
+
+@app.route("/tools/news/")
+def news():
+    """News."""
+    return render_template("tools/news.html")
+
+
+@app.route("/tools/libertarian-zoo/")
+def libertarian_zoo():
+    """Libertarian Zoo."""
+    return render_template("tools/libertarian-zoo/index.html")
+
+
+@app.route("/tools/libertarian-zoo/2020/")
+def libertarian_zoo_2020():
+    """Libertarian Zoo 2020."""
+    return render_template("tools/libertarian-zoo/2020.html")
+
+
+@app.route("/tools/jwt/")
+def jwt():
+    """JWT."""
+    return render_template("tools/jwt.html")
+
+
+@app.route("/tools/sponge/")
+def sponge():
+    """Sponge."""
+    return render_template("tools/sponge.html")
+
+
+@app.route("/tools/dencode/")
+def dencode():
+    """Dencode."""
+    return render_template("tools/dencode.html")
+
+
+@app.route("/tools/query-dumper/")
+def query_dumper():
+    """Query Dumper."""
+    return render_template("tools/query-dumper.html")
 
 
 @app.route("/robots.txt")
@@ -31,6 +73,12 @@ def sitemap():
     """Sitemap."""
     # TODO
     return '<?xml version="1.0" encoding="UTF-8"?>'
+
+
+@app.route("/favicon.ico")
+def favicon_ico():
+    """Favicon."""
+    return app.send_static_file("images/favicon.ico")
 
 
 @app.errorhandler(404)
