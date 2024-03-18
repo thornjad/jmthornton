9867755448c8540b39a44326d45593e6b4c03c67
Jade Michael Thornton <jademichael@jmthornton.net>
2024-03-18 16:19:36 -0400

    not working

--- src/jmthornton/web.clj
+++ src/jmthornton/web.clj
@@ -1,7 +1,23 @@
 (ns jmthornton.web
-  (:require [stasis.core :as stasis]))
+  (:require [stasis.core :as stasis]
+            [clojure.java.io :as io]
+            [hiccup2.core :refer [html]]))
+
+(defn layout-page [page]
+  (html
+   [:html
+    [:head
+     [:title "My new site"]
+     [:link {:rel "stylesheet" :href "/assets/styles/main.css"}]]
+    [:body
+     [:h1 "Dynamically inserted stuff"]
+     [:div.body page]]]))
+
+(defn about-page [request]
+  (layout-page (slurp (io/resource "partials/about.html"))))
 
 (defn get-pages []
-  (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$"))
+  (merge (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
+         {"/about/" about-page}))
 
 (def app (stasis/serve-pages get-pages))
