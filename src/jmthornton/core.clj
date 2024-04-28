381ef8ecdb3753afb9e99cb66d3a12671157d682
Jade Michael Thornton <jademichael@jmthornton.net>
2024-04-28 16:26:24 -0400

    cleanup

--- src/jmthornton/core.clj
+++ src/jmthornton/core.clj
@@ -1,24 +1,10 @@
 (ns jmthornton.core
-  (:require [clojure.java.io :as io]
-            [hiccup.page :refer [html5]]
-            [ring.util.response :refer [response]]
+  (:require [hiccup.page :refer [html5]]
             [stasis.core :as stasis]))
 
-(defn layout-page [page]
-  (html5
-   [:html
-    [:head
-     [:title "My new site"]
-     [:link {:rel "stylesheet"
-             :href "/assets/styles/main.css"}]]
-    [:body
-     [:h1 "Dynamically inserted stuff"]
-     [:div.body page]]]))
-
 (defn frontpage [context]
-  (response
-   (html5
-    [:div "Hello world"])))
+  (html5
+   [:div "Hello world"]))
 
 (defn get-pages []
   {"/" frontpage})
