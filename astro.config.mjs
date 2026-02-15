681b6beca6573ab485947d0abcd0ecd9965b527c
Jade Michael Thornton <jademichael@jmthornton.net>
2026-02-15 10:30:11 -0600

    remove adapter for static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare(),
   prefetch: true,
   outDir: 'dist',
 });
